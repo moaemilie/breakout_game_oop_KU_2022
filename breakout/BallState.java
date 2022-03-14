21859e0b4fa9634772dc45cf8c046964be65dd05
moaemilie <emilie.g.l@hotmail.com>
2022-03-14 13:20:42 +0100

    Started editing BallState class

--- breakout/BallState.java
+++ breakout/BallState.java
@@ -1,13 +1,44 @@
 package breakout;
 
+import java.awt.Point;
+
+/**
+ * 
+ * @invar ??
+ * 
+ * 
+ */
+
 public class BallState {
-	// TODO: implement
 	
-	public Point getCenter() {
-		return null;
+	/**
+	 * 
+	 */
+	private Point center;
+	private Vector INIT_BALL_VELOCITY;
+	private int diameter;
+	
+	/**
+	 * @post | getCenter() == center
+	 * @post | getVelocity() == INIT_BALL_VELOCITY
+	 */
+	public BallState(Point center, diameter, Vector INIT_BALL_VELOCITY) {
+		this.center = center; 
+		this.diameter = diameter;
+		this.INIT_BALL_VELOCITY = INIT_BALL_VELOCITY;
 	}
 	
+	/**
+	 * Returns the center of the ball
+	 */
+	public Point getCenter() {
+		return center;
+	}
+	
+	/**
+	 * Returns the velocity of the ball
+	 */
 	public Vector getVelocity() {
-		return null;
+		return INIT_BALL_VELOCITY;
 	}
 }
