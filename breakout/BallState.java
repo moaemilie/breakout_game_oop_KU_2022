2fe2c4c4d0d9a3ccc7c01105c006e449b33b903a
Kjersti <kjkv@nmbu.no>
2022-03-16 13:51:44 +0100

    Added some documentation to BallState

--- breakout/BallState.java
+++ breakout/BallState.java
@@ -3,26 +3,38 @@ package breakout;
 import java.awt.Point;
 
 /**
- * 
- * @invar ??
- * 
- * 
+ * This class represents a ball with a given ´diameter´, ´center´and ´INIT_BALL_VELOCITY´.
+ *  
+ * @immutable
  */
 
 public class BallState {
 	
 	/**
-	 * 
+	 * @invar | center.getX() > 0
+	 * @invar | center.getY() > 0
+	 * @invar | diameter > 0
 	 */
 	private Point center;
 	private Vector INIT_BALL_VELOCITY;
 	private int diameter;
 	
 	/**
+	 * @throws IllegalArgumentException | diameter <= 0 
+	 * @throws IllegalArgumentException | center.getY() > 0
+	 * @throws IllegalArgumentException | center.getX() > 0
+	 * 
 	 * @post | getCenter() == center
 	 * @post | getVelocity() == INIT_BALL_VELOCITY
 	 */
 	public BallState(Point center, int diameter, Vector INIT_BALL_VELOCITY) {
+		if (diameter <= 0)
+			throw new IllegalArgumentException();
+		if (center.getY() > 0)
+			throw new IllegalArgumentException();
+		if (center.getX() > 0)
+			throw new IllegalArgumentException();
+		
 		this.center = center; 
 		this.diameter = diameter;
 		this.INIT_BALL_VELOCITY = INIT_BALL_VELOCITY;
